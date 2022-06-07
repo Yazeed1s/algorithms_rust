2593154d8e8aa089021e9d8e5806eee5686b6660
Yazeed1s <Yazeed1s@users.noreply.github.com>
2022-06-07 01:06:47 -0400

    added tests to bubble_sort

--- bubble_sort/src/lib.rs
+++ bubble_sort/src/lib.rs
@@ -1,25 +1,38 @@
-#[cfg(test)]
-mod tests {
-    fn bubble_sort(nums: &mut [i32]) -> &mut [i32] {
-        let mut swapped: bool;
-        for i in 0..nums.len() {
-            swapped = false;
-            for j in 0..nums.len() - i - 1 {
-                if nums[j] > nums[j + 1] {
-                    nums.swap(j, j + 1);
-                    swapped = true;
-                }
-            }
-            if !swapped {
-                break;
+#[allow(dead_code)]
+fn bubble_sort<T: std::cmp::Ord>(nums: &mut [T]) -> &mut [T] {
+    let mut swapped: bool;
+    for i in 0..nums.len() {
+        swapped = false;
+        for j in 0..nums.len() - i - 1 {
+            if nums[j] > nums[j + 1] {
+                nums.swap(j, j + 1);
+                swapped = true;
             }
         }
-        nums
+        if !swapped {
+            break;
+        }
     }
+    nums
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Rng;
     #[test]
-    fn it_works() {
+    fn basic_test() {
         let nums: &mut [i32; 5] = &mut [2, 3, 4, 1, 5];
         let _sorted: &mut [i32] = bubble_sort(nums);
         assert_eq!(_sorted, [1, 2, 3, 4, 5]);
+    }
+    #[test]
+    fn test_bubble_sort_1000s() {
+        let mut rng = rand::thread_rng();
+        let nums: &mut [i32; 10000] = &mut [rng.gen(); 10000];
+        bubble_sort(nums);
+        for i in 0..nums.len() - 1 {
+            assert!(nums[i] <= nums[i + 1]);
+        }
     }
 }
