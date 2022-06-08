7e2b423286e424a9c968e39490d8ea6a71cc4bd6
Yazeed1s <106896327+Yazeed1s@users.noreply.github.com>
2022-06-08 13:44:23 -0400

    Delete binary_search directory

--- binary_search/target/debug/deps/binary_search-c209ad4092a39fff.d
+++
@@ -1,7 +0,0 @@
-/Users/yazeed_1/Documents/Coding/rust_dev/algorithms_rust/binary_search/target/debug/deps/binary_search-c209ad4092a39fff.rmeta: src/lib.rs
-
-/Users/yazeed_1/Documents/Coding/rust_dev/algorithms_rust/binary_search/target/debug/deps/binary_search-c209ad4092a39fff.d: src/lib.rs
-
-src/lib.rs:
-
-# env-dep:CLIPPY_ARGS=
