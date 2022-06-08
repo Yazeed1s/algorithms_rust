26fc6b1c2ed8bc40e97decbb9152195ed043fa85
Yazeed1s <Yazeed1s@users.noreply.github.com>
2022-06-08 13:42:53 -0400

    added binary_search

---
+++ binary_search/target/debug/deps/binary_search-2d22499f64327f6d.d
@@ -0,0 +1,7 @@
+/Users/yazeed_1/Documents/Coding/rust_dev/algorithms_rust/binary_search/target/debug/deps/binary_search-2d22499f64327f6d.rmeta: src/lib.rs
+
+/Users/yazeed_1/Documents/Coding/rust_dev/algorithms_rust/binary_search/target/debug/deps/binary_search-2d22499f64327f6d.d: src/lib.rs
+
+src/lib.rs:
+
+# env-dep:CLIPPY_ARGS=
