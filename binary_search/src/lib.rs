4a70a46d9b878538d3da67c1d9a3eeb4a1f14e84
Yazeed1s <Yazeed1s@users.noreply.github.com>
2022-06-08 23:59:58 -0400

    implemented binary_search

--- binary_search/src/lib.rs
+++ binary_search/src/lib.rs
@@ -1,8 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub fn binary_search<T>(nums: &[T], key: T) -> Result<usize,usize>
+where
+    T: std::cmp::Ord,
+{
+    let mut low: i32 = 0;
+    let mut high: i32 = nums.len() as i32 - 1;
+    while low <= high {
+        let mid: i32 = (((high + low) / 2) as f64).floor() as i32;
+        if let Some(index) = nums.get(mid as usize) {
+            if *index == key {
+                return Ok(mid as usize);
+            }
+            if *index > key {
+                high = mid - 1;
+            }
+            if *index < key {
+                low = mid + 1;
+            }
+        }
     }
+    Err(0)
 }
