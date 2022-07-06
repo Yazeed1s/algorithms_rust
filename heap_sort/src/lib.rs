428279b2b4c2d4df29b9ba5b81ec9add5e9bccd4
Yazeed1s <Yazeed1s@users.noreply.github.com>
2022-07-06 15:22:51 -0400

    added tests

--- heap_sort/src/lib.rs
+++ heap_sort/src/lib.rs
@@ -1,29 +1,29 @@
-pub fn heapsort<T: Ord>(arr: &mut [T]) {
-    let end = arr.len();
+pub fn heap_sort<T: std::cmp::Ord>(nums: &mut [T]) -> &[T] {
+    let end = nums.len();
     for start in (0..end / 2).rev() {
-       
-        sift_down(arr, start, end - 1);
+        move_down(nums, start, end - 1);
     }
-    for end in (1..arr.len()).rev() {
-        arr.swap(end, 0);
-        sift_down(arr, 0, end - 1);
+    for end in (1..nums.len()).rev() {
+        nums.swap(end, 0);
+        move_down(nums, 0, end - 1);
     }
+    nums
 }
 
-fn sift_down<T: Ord>(arr: &mut [T], start: usize, end: usize) {
+fn move_down<T: std::cmp::Ord>(nums: &mut [T], start: usize, end: usize) {
     let mut root = start;
     loop {
         let mut child = root * 2 + 1; // Get the left child
         if child > end {
             break;
         }
-        if child < end && arr[child] < arr[child + 1] {
+        if child < end && nums[child] < nums[child + 1] {
             // Right child exists and is greater.
             child += 1;
         }
-        if arr[root] < arr[child] {
+        if nums[root] < nums[child] {
             // If child is greater than root, swap'em!
-            arr.swap(root, child);
+            nums.swap(root, child);
             root = child;
         } else {
             break;
@@ -32,9 +32,49 @@ fn sift_down<T: Ord>(arr: &mut [T], start: usize, end: usize) {
 }
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use rand::Rng; // add -> rand = "0.8.5" under [dependencies] in Cargo.toml
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_reverced_slice() {
+        let mut nums = [0u16; 100];
+        rand::thread_rng().fill(&mut nums[..]);
+        nums.reverse();
+        heap_sort(&mut nums);
+        for i in 0..nums.len() - 1 {
+            assert!(nums[i] <= nums[i + 1]);
+        }
+    }
+    #[test]
+    fn test_shell_sort_with_100_random_u16s() {
+        let mut nums = [0u16; 100];
+        rand::thread_rng().fill(&mut nums[..]);
+        heap_sort(&mut nums);
+        for i in 0..nums.len() - 1 {
+            assert!(nums[i] <= nums[i + 1]);
+        }
+    }
+
+    #[test]
+    fn test_shell_sort_with_slice_of_chars() {
+        let mut chars: [char; 26] = [
+            'q', 'a', 'z', 'x', 's', 'w', 'e', 'd', 'c', 'r', 'f', 'v', 't', 'g', 'b', 'y', 'h',
+            'n', 'u', 'j', 'm', 'i', 'k', 'o', 'l', 'p',
+        ];
+        heap_sort(&mut chars);
+        for i in 0..chars.len() - 1 {
+            assert!(chars[i] <= chars[i + 1]);
+        }
+    }
+
+    #[test]
+    fn test_shell_sort_with_strings() {
+        let mut strs: [&str; 26] = [
+            "q", "a", "z", "x", "s", "w", "e", "d", "c", "r", "f", "v", "t", "g", "b", "y", "h",
+            "n", "u", "j", "m", "i", "k", "o", "l", "p",
+        ];
+        heap_sort(&mut strs);
+        for i in 0..strs.len() - 1 {
+            assert!(strs[i] <= strs[i + 1]);
+        }
     }
 }
